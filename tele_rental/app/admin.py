01c799a085f3d9972de0b2458c2b72e463d277a6
nilondes <oxygen93@ya.ru>
2024-10-04 15:11:49 +0300

    added search ad logic and search criteria

--- tele_rental/app/admin.py
+++ tele_rental/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Ad
+from .models import User, Ad, SearchCriteria
 
 
 admin.site.register(Ad)
 admin.site.register(User)
+admin.site.register(SearchCriteria)
--- tele_rental/app/models.py
+++ tele_rental/app/models.py
@@ -1,5 +1,8 @@
+from email.policy import default
+
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(models.Model):
@@ -18,10 +21,19 @@ class Ad(models.Model):
     location = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField()
-    user = models.ForeignKey(User, to_field='username', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, to_field='username', on_delete=models.CASCADE, db_column='user')
     phone = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     is_approved = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name} - {self.user} - {self.created_at}'
+
+class SearchCriteria(models.Model):
+    chat_id = models.ForeignKey(User, to_field='chat_id', on_delete=models.CASCADE, db_column='chat_id')
+    keywords = ArrayField(ArrayField(models.CharField(max_length=40), default=('%%', ), blank=True))
+    min_price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    max_price = models.DecimalField(max_digits=7, decimal_places=2, default=99999.99)
+
+    def __str__(self):
+        return f'{self.chat_id}'
